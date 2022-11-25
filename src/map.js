5ac98fccc9bab3587121b7cc851573906ea95432
Joseph <josephsullivan256@gmail.com>
2022-11-25 02:45:47 -0800

    added z_depth method to map.js

--- src/map.js
+++ src/map.js
@@ -7,6 +7,15 @@ export default class TileMap {
 		this.n = 24;
 	}
 
+	*z_depth(z){
+		for(let j = 0; j < Math.min(z+1,2*this.n-z-1); j++){
+			let x = j - (Math.min(z+1,2*this.n-z-1)/2.0);
+			let y = z/2.0;
+
+			yield [this.tile_image, x, y]
+		}
+	}
+
 	draw(ctx) {
 		ctx.save();
 
@@ -16,11 +25,8 @@ export default class TileMap {
 		// iterate the diagonal cross sections in case a tile extends outside of its boundaries
 		// otherwise, we would have z issues
 		for(let i = 0; i < 2*this.n-1; i++){
-			for(let j = 0; j < Math.min(i+1,2*this.n-i-1); j++){
-				let x = j - (Math.min(i+1,2*this.n-i-1)/2.0);
-				let y = i/2.0;
-
-				ctx.drawImage(this.tile_image, x*28, y*16);
+			for(let [tile, x, y] of this.z_depth(i)){
+				ctx.drawImage(tile, x*28, y*16);
 			}
 		}
 
